9765b06e4d302155e306bc21bb212ac5c627d518
Rupal Nahar <rupal.nahar@oracle.com>
2021-08-03 17:28:57 -0400

    setup.py change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     entry_points={
         'console_scripts': [
-            'ocid_precommit = ocid_precommit:main',
+            'ocid_precommit = ocid_precommit_check:main',
         ],
     },
 )
